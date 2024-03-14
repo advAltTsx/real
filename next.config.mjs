f792c467f1f9f5e6994f594abb75ffadf0ce1230
Vasooli Bhai <154480782+advAltTsx@users.noreply.github.com>
2024-03-14 13:39:33 +0530

    *

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: 'https://irl.abhyudaya.xyz',
-            destination: '/home',
-            permanent: true,
-          },
-        ]
-      },
+    
 };
 
 export default nextConfig;
