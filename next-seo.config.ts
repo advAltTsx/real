1a22acd3cfcbba163d4d11e97072af2c29992fdc
Vasooli Bhai <154480782+advAltTsx@users.noreply.github.com>
2024-03-20 12:36:59 +0530

    (

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,6 +1,6 @@
 import { NextSeoProps } from 'next-seo';
 
 export default {
-  title: 'Abhyudaya',
-  description: "deepabhyudaya's personal website.",
+  title: 'Abhyudaya - Home',
+  description: "Deep Abhyudaya's personal website.",
 } as NextSeoProps;
